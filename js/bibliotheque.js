f366d4336041070246769e273a84eb1a73cec848
marc-bouvier <m.bouvier.dev@gmail.com>
2017-05-25 19:17:46 +0200

    ajout de la bibliotheque

---
+++ js/bibliotheque.js
@@ -0,0 +1,62 @@
+var data = [{
+        id: "the_pragmatic_programmer",
+        title: "The Pragmatic Programmer",
+        subTitle: "from journeyman to master",
+        authors: ["Andrew Hunt", "David Thomas"],
+        cover: "img/bibliotheque/the-pragmatic-programmer.jpg",
+        isbn13: "9780201616224",
+        language: "Anglais",
+        editor: "Addison-Wesley",
+        date: 1999
+    },
+    {
+        id: "domain_driven_design",
+        title: "Domain-Driven-Design",
+        subTitle: "Tackling Complexity in the heart of Software",
+        authors: ["Eric Evans"],
+        cover: "img/bibliotheque/domain-driven-design.jpg",
+        isbn13: "9780321125217",
+        language: "Anglais",
+        editor: "Addison-Wesley",
+        date: 2003
+    }, {
+        id: "joel_on_software",
+        title: "Joel on Software",
+        subTitle: "And on diverse and Occasionally Related Matters That Will Propve of Interest to Software Developers, Designers, and Managers, and to Those Who, Whether by Good Fortune or Ill Luck, Work with Them in Some Capacity",
+        authors: ["Joel Spolsky"],
+        cover: "img/bibliotheque/joel-on-software.jpg",
+        isbn13: "9781590593899",
+        language: "Anglais",
+        editor: "Apress",
+        date: 2004
+    }, {
+        id: "peaa",
+        title: "Patterns of Enterprise Application Architecture",
+        authors: ["Martin Fowler", "David Rice", "Matthew Foemmel", "Edward Hieatt", "Robert Mee", "Randy Stafford"],
+        cover: "img/bibliotheque/peaa.jpg",
+        isbn13: "9780321127426",
+        language: "Anglais",
+        editor: "Addison-Wesley",
+        date: 2002
+    }, {
+        id: "rupture-douce-1",
+        title: "Rupture Douce - saison 1",
+        subTitle: "Petites Histoires de Grenouilles Agiles, à l'usage des défricheurs de néo-mondes organisationnels, des métamorphoseurs d'équipes, des osthéopathes de projets à miraculer, bref ... des agilitateurs!",
+        authors: ["Laurent Sarrazin", "contributions collectives"],
+        cover: "img/bibliotheque/rupture-douce-1.webp",
+        isbn13: "9781291151596",
+        language: "Français",
+        editor: "lulu.com",
+        date: 2012
+    }, {
+        id: "rupture-douce-2",
+        title: "Rupture Douce - saison 2",
+        subTitle: "Les Grenouilles Agiles Reviennent ... avec un Nouveau Cocktail de Perturbations Positives et comme Noble Ambition : Transcender l'Agilité pour Energiser le Développement du Capital Humain ET la Performance. Vous goûtez?",
+        authors: ["Laurent Sarrazin", "contributions collectives"],
+        cover: "img/bibliotheque/rupture-douce-2.webp",
+        isbn13: "9781291423273",
+        language: "Français",
+        editor: "lulu.com",
+        date: 2013
+    }
+]
